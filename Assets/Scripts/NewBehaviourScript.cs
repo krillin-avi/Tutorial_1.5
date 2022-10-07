2b26f53a4fa7cc3f83fd58682a21a96de6f1a634
krillin-avi <cesdaille@knights.ucf.edu>
2022-10-07 12:32:48 -0400

    Animation commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,10 +7,11 @@ public class NewBehaviourScript : MonoBehaviour
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
     public AudioSource musicSource;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+      anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,12 +21,14 @@ public class NewBehaviourScript : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
 
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
         }
 
@@ -33,11 +36,13 @@ public class NewBehaviourScript : MonoBehaviour
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
 
         }
 
